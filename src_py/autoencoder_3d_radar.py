f3a1a4392c7a639a71ed5ba92c17cb495955c4c0
Tsuyoshi Inoue <inoue0406@gmail.com>
2017-05-14 00:15:34 +0900

    downsampled autoencoder due to memory limit

---
+++ src_py/autoencoder_3d_radar.py
@@ -0,0 +1,95 @@
+'''
+Apply autoencoder for 3d radar echo data.
+'''
+
+from keras.layers import Input, Dense
+from keras.models import Model
+import numpy as np
+import h5py
+import os
+
+#----------
+os.chdir('C:/home/CIKM2017')
+
+N = 10000
+nx = 34
+ny = 34
+nz = 4
+
+# read data
+h5file = h5py.File('processed/for_python/radar_train_3d_ds3.hdf5','r')
+x_train =  h5file['MR'].value
+x_train =  x_train/np.max(x_train) # regularize to [0-1]
+h5file.close()
+# 
+h5file = h5py.File('processed/for_python/radar_testA_3d_ds3.hdf5','r')
+x_test =  h5file['MR'].value
+x_test =  x_test/np.max(x_test) # regularize to [0-1]
+h5file.close()
+
+# ---------------
+input_img = Input(shape=(nx*ny*nz,))
+
+encoded = Dense(1024, activation='relu')(input_img)
+encoded = Dense(512, activation='relu')(encoded)
+encoded = Dense(256, activation='relu')(encoded)
+encoded = Dense(128, activation='relu')(encoded)
+
+decoded = Dense(256, activation='relu')(encoded)
+decoded = Dense(512, activation='relu')(decoded)
+decoded = Dense(1024, activation='relu')(decoded)
+decoded = Dense(nx*ny, activation='sigmoid')(decoded)
+
+autoencoder = Model(inputs=input_img, outputs=decoded)
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+# fitting
+autoencoder.fit(x_train, x_train,
+                epochs=1000,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+# save
+autoencoder.save_weights('autoencoder.h5')
+autoencoder.load_weights('autoencoder.h5')
+
+# output middle layer 
+intermediate_model = Model(inputs=autoencoder.input, 
+                           outputs=autoencoder.layers[3].output)
+intermediate_model.compile(optimizer='adadelta', loss='binary_crossentropy')
+intermediate_output = intermediate_model.predict(x_train)
+
+h5file = h5py.File('res/autoencoder_2d/auto_2d_feature128_0513.hdf5','w')
+h5file.create_dataset('MR',data= intermediate_output)
+h5file.close()
+
+# ------------------------------------
+# plotting
+# ------------------------------------
+import matplotlib.pyplot as plt
+
+# テスト画像を変換
+decoded_imgs = autoencoder.predict(x_test)
+
+# 何個表示するか
+n = 20
+plt.figure(figsize=(40, 8))
+for i in range(n):
+    # オリジナルのテスト画像を表示
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(x_test[i].reshape(34, 34))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # 変換された画像を表示
+    ax = plt.subplot(2, n, i+1+n)
+    plt.imshow(decoded_imgs[i].reshape(34, 34))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
+
---
+++ src_py/prep_data_2dencoder_down.py
@@ -0,0 +1,76 @@
+'''
+Apply autoencoder for 2d radar echo data.
+With downsampling
+'''
+
+import numpy as np
+import h5py
+import os
+
+#----------
+os.chdir('C:/home/CIKM2017')
+
+N = 10000
+#nx = 101
+#ny = 101
+nx = 34
+ny = 34
+nz = 4
+
+x_train2d = np.zeros((N,nx*ny),dtype=np.float32)
+x_train3d = np.zeros((N,nx*ny*nz),dtype=np.float32)
+
+for n in range(10000):
+    fname = 'processed/train_h5/radar_train_%05d.hdf5' % (n+1)
+    print(n,fname)
+    file = h5py.File(fname, 'r') 
+    data =  file['MR']
+    #---------------
+    # array size should be (# samples, data length)
+    x = data[:,:,0,14]
+    x3 = data[:,:,:,14]
+    x = x[::3,::3] # downsample
+    x3 = x3[::3,::3,:] # downsample
+    x_train2d[n,:] = x.reshape(1,nx*ny)
+    x_train3d[n,:] = x3.reshape(1,nx*ny*nz)
+    # 
+    file.close()
+
+h5file = h5py.File('processed/for_python/radar_train_2d_ds3.hdf5','w')
+h5file.create_dataset('MR',data= x_train2d)
+h5file.close()
+
+h5file = h5py.File('processed/for_python/radar_train_3d_ds3.hdf5','w')
+h5file.create_dataset('MR',data= x_train3d)
+h5file.close()
+
+# test set
+
+N = 2000
+x_testA2d = np.zeros((N,nx*ny),dtype=np.float32)
+x_testA3d = np.zeros((N,nx*ny*nz),dtype=np.float32)
+
+for n in range(2000):
+    fname = 'processed/testA_h5/radar_testA_%05d.hdf5' % (n+1)
+    print(n,fname)
+    file = h5py.File(fname, 'r') 
+    data =  file['MR']
+    #---------------
+    # array size should be (# samples, data length)
+    x = data[:,:,0,14]
+    x3 = data[:,:,:,14]
+    x = x[::3,::3] # downsample
+    x3 = x3[::3,::3,:] # downsample
+    x_testA2d[n,:] = x.reshape(1,nx*ny)
+    x_testA3d[n,:] = x3.reshape(1,nx*ny*nz)
+    # 
+    file.close()
+
+h5file = h5py.File('processed/for_python/radar_testA_2d_ds3.hdf5','w')
+h5file.create_dataset('MR',data= x_testA2d)
+h5file.close()
+
+h5file = h5py.File('processed/for_python/radar_testA_3d_ds3.hdf5','w')
+h5file.create_dataset('MR',data= x_testA3d)
+h5file.close()
+
